c1082173ea24f7a414fd8f63f06859f845dd50fe
AntonUniatitskyi <antonh1803@gmail.com>
2026-09-11 16:25:26 -0400

    Add: add server component

---
+++ server.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from EmotionDetection import emotion_detector
+
+app = Flask(__name__)
+
+@app.route("/")
+def render_index_page():
+    return render_template('index.html')
+
+@app.route("/emotionDetector")
+def sent_analyzer():
+    text_to_analyze = request.args.get('textToAnalyze')
+    response = emotion_detector(text_to_analyze)
+    
+    anger = response['anger']
+    disgust = response['disgust']
+    fear = response['fear']
+    joy = response['joy']
+    sadness = response['sadness']
+    dominant_emotion = response['dominant_emotion']
+    
+    return (
+        f"Для даного висловлення відповідь системи: "
+        f"'anger': {anger}, 'disgust': {disgust}, 'fear': {fear}, "
+        f"'joy': {joy} та 'sadness': {sadness}. "
+        f"Домінуюча емоція - {dominant_emotion}."
+    )
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
